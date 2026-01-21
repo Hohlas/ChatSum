1ef49ab48b8506d5aee779209f79b364ac331a09
hohlas <mail@hohla.ru>
2026-01-21 17:31:44 +0300

    'disable_search': True

--- main.py
+++ main.py
@@ -947,7 +947,8 @@ async def create_summary(messages_data, chat_id_str, model='sonar', use_reasonin
                 {'role': 'user', 'content': user_content}
             ],
             'temperature': 0.3,
-            'max_tokens': 9000
+            'max_tokens': 12000,
+            'disable_search': True
         }
         
         # Выводим информацию о размере запроса
